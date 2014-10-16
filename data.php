b02d681b2f8df7c1a6ea152e9ae8319c6099c8a8
Adam <ajlfh7@mail.missouri.edu>
2014-10-16 15:52:59 -0500

    add data and edit index

---
+++ data.php
@@ -0,0 +1,7 @@
+<?php
+DEFINE("HOST", "host=dbhost-pgsql.cs.missouri.edu");
+DEFINE("DBNAME", "dbname=testdb");
+DEFINE("USERNAME", "user=testuser");
+DEFINE("PASSWORD","password=testPW");
+?>
+
--- index.php
+++ index.php
@@ -33,6 +33,34 @@
 </head>
 
 <body>
+<?php
+
+include ("data.php");
+
+if(isset($_POST['submit'])) {
+	
+	
+		
+	$conn = pg_connect(HOST." ".DBNAME." ".USERNAME." ".PASSWORD) or die('Could not connect:'. pg_last_error());
+	if (!$conn) 
+	{
+		echo "<br/>An error occurred with connecting to the server.<br/>";
+		die();
+	}
+
+	 //select all info based on email
+	$query = "SELECT * FROM careerschema.students WHERE email = $1";
+	$stmt = pg_prepare($conn, "qrQuery", $query);
+	//sends query to database
+	$result = pg_execute($conn, "qrQuery", array($_POST['email']));
+	//if database doesnt return results print this
+	if(!$result) {
+			echo 'I didnt work<br />';
+	}
+
+}
+?>
+
 <div class="container">
 	<div class="row clearfix">
 		<div class="col-md-12 column">
